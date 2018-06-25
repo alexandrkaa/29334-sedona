40b7d972544f51088bfe20cf41d9eb68a81fe712
alexandrkaa <komar81@gmail.com>
2018-06-25 12:52:39 +0300

    убрал открытие / закрытие popup (в IE глючит)

--- js/index.js
+++
@@ -1,6 +0,0 @@
-var searchHotelButton = document.querySelector('.search-hotel .search-hotel-btn');
-var searchHotelPopUpForm = document.querySelector('.search-hotel-popup-form');
-searchHotelButton.addEventListener( "click" , function(e) {
-  e.preventDefault();
-  searchHotelPopUpForm.classList.toggle("open");
-});
